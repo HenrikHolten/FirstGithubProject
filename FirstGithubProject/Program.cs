ac3f9d7bf5b716229980fd885e7884f7944e3353
Henrik <Henrik@HENRIK-PRIVATE>
2016-07-10 16:46:29 +0200

    Add project files.

---
+++ FirstGithubProject/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstGithubProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("It's Alive");
+            Console.ReadKey();
+        }
+    }
+}
